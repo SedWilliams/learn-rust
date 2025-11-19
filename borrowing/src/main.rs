9ea253fd89879f2e5fd9512da405ce6beaed74e9
tre3iii <lilshed3@gmail.com>
2025-11-18 23:27:17 -0800

    update

---
+++ borrowing/src/main.rs
@@ -0,0 +1,15 @@
+
+
+
+fn main() {
+
+    /*********************************
+     * Borrowing (pointers/references)
+     * - For if you want to use a value without taking ownership of it
+     * - References -> pointers to the reference holding the pointer to the values location in mem.
+     * - This prevents 'data races' see rust book, section 4.2
+     * - References go in and out of scope independently of the data they point to
+     *********************************/
+
+    
+}
